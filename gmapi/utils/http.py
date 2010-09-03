ec14b30383e43116b9ca5ea620320918db0d6cd8
David Bennett <ungenio@gmail.com>
2010-09-03 15:36:14 -0500

    Redo custom urlencode.

--- gmapi/utils/http.py
+++ gmapi/utils/http.py
@@ -1,15 +1,20 @@
-import urllib
+from django.utils.http import urlquote_plus
 
 
-def urlencode(query, doseq=0):
-    """Custom urlencode that leaves static map delimiters alone."""
-    url = urllib.urlencode(query, doseq)
-    def parse(p):
-        # Process each key and value.
-        p = map(lambda s:
-                # Unquote then quote each item.
-                urllib.quote_plus(urllib.unquote_plus(s), '|,:'),
-                p.split('='))
-        return '='.join(p)
-    # Process each key-value pair.
-    return '&'.join(map(parse, url.split('&')))
+def urlencode(query, doseq=0, safe=''):
+    """Custom urlencode that leaves static map delimiters ("|", ",", ":") alone.
+
+    Based on Django's unicode-safe version of urllib.quote_plus.
+
+    """
+    safe = safe + '|,:'
+    if hasattr(query, 'items'):
+        query = query.items()
+    if not doseq:
+        return '&'.join([urlquote_plus(k, safe) + '=' + urlquote_plus(v, safe)
+                         for k, v in query])
+    else:
+        return '&'.join([urlquote_plus(k, safe) + '=' + urlquote_plus(v, safe)
+                         for k, s in query
+                         for v in ((isinstance(s, basestring) and [s]) or
+                                   (hasattr(s, '__len__') and s) or [s])])
